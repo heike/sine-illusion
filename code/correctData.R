10662ec0fc5a64867a0a77dc44b5834c89b4d3f9
Susan VanderPlas <srvanderplas@gmail.com>
2013-10-02 12:19:10 -0500

    X correction function now works

--- code/correctData.R
+++ code/correctData.R
@@ -1,21 +1,44 @@
 # Functions to correct actual data in x and y
+library(ggplot2)
+# correct.x <- function(x, y, data, weight=.3595){ 
+#   # since .3595 is the midpoint of the left and right estimates of optimal values.
+#   require(locfit)
+#   form <- as.formula(paste(y, "~lp(", x, ", deg=2, acri=)"))
+#   
+#   model <- locfit(data=data, formula=form)
+#   deriv <- locfit(data=data, formula=form, deriv=1)
+#   f <- function(z) predict(model, newdata=structure(data.frame(x=z, y=0), names=c(x, y)))
+#   fprime <- function(z) predict(deriv, newdata=structure(data.frame(x=z, y=0), names=c(x,y)))
+#   a <- min(data[,x])
+#   b <- max(data[,x])
+#   
+#   const <- integrate(function(z) abs(fprime(z)), a, b)$value
+#   trans <- sapply(data[,x], function(i) integrate(function(z) abs(fprime(z)), a, i)$value*(b-a)/const + a)
+# #   const <- sum(abs(fprime(data[,x])))
+# #   trans <- cumsum(abs(fprime(data[,x])))*(b-a)/const+a                  
+#   
+#   data[,paste(x, "correctx", sep=".")] <- trans*weight + data[,x]*(1-weight)
+# 
+#   data
+# }
 
 correct.x <- function(x, y, data, weight=.3595){ 
   # since .3595 is the midpoint of the left and right estimates of optimal values.
-  require(locfit)
-  form <- as.formula(paste(y, "~", x))
-  model <- locfit(data=data, formula=form)
-  deriv <- locfit(data=data, formula=form, deriv=1)
-  f <- function(z) predict(model, newdata=structure(data.frame(x=z, y=0), names=c(x, y)))
-  fprime <- function(z) predict(deriv, newdata=structure(data.frame(x=z, y=0), names=c(x,y)))
+  
+  model <- smooth.spline(data[,x], data[,y], df=floor(length(unique(data[,x]))/2))
+
+  f <- function(z) predict(model, x=z)$y
+  fprime <- function(z) abs(predict(model, x=z, deriv=1)$y)
   a <- min(data[,x])
   b <- max(data[,x])
-  const <- integrate(function(z) abs(fprime(z)), a, b)$value
+  
+  const <- integrate(function(z) fprime(z), a, b, subdivisions=10000)$value
   trans <- sapply(data[,x], function(i) integrate(function(z) abs(fprime(z)), a, i)$value*(b-a)/const + a)
-                  
+  #   const <- sum(abs(fprime(data[,x])))
+  #   trans <- cumsum(abs(fprime(data[,x])))*(b-a)/const+a                  
   
   data[,paste(x, "correctx", sep=".")] <- trans*weight + data[,x]*(1-weight)
-
+  
   data
 }
 
@@ -56,8 +79,8 @@ correct <- function(x, y, data, type){
   else correct.y(x, y, data)
 }
 
-datasub <- read.csv("data/Ozone/Ozone-subset.csv")
-temp <- correct("Tmax", "Ozone", datasub, type="x")
-qplot(data=temp, x=Tmax.correctx, y=Ozone)
-temp2 <- correct("Tmax", "Ozone", datasub, type="y")
-qplot(data=temp2, x=Tmax, y=Ozone.correcty)
+# datasub <- read.csv("data/Ozone/Ozone-subset.csv")
+# temp <- correct("Tmax", "Ozone", datasub, type="x")
+# qplot(data=temp, x=Tmax.correctx, y=Ozone)
+# temp2 <- correct("Tmax", "Ozone", datasub, type="y")
+# qplot(data=temp2, x=Tmax, y=Ozone.correcty)
